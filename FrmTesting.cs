df5cd9f5762374bfca23c3a1b47eaeebc325ba34
Joseph Fischetti <fischettijw@gmail.com>
2020-06-12 07:58:50 -0400

    Add project files.

---
+++ FrmTesting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateUIviaSeparteThread
+{
+    public partial class FrmTesting : Form
+    {
+        public delegate void DelegateTxtboxUI(string str); // Delegate with Signature
+        ThreadStart threadStart;
+        Thread updateTxtboxUI;
+
+        public FrmTesting()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTesting_Load(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.Name = "PrimaryThread";
+
+            threadStart = new ThreadStart(GetTheThreadStarted); //ThreadStart object defining Method to call
+            updateTxtboxUI = new Thread(threadStart);           //Thread object defining ThreadStart object
+            updateTxtboxUI.Name = "SecondaryThread";
+        }
+
+        private void GetTheThreadStarted()
+        {
+            DelegateTxtboxUI myDelegateUI = new DelegateTxtboxUI(UpdateTextbox);
+            this.Txt01.BeginInvoke(myDelegateUI, $"Updated from THREAD: {Thread.CurrentThread.Name}");
+        }
+
+        private void UpdateTextbox(string str)
+        {
+            this.Txt01.Text = str;
+        }
+
+        private void BtnClick_Click(object sender, EventArgs e)
+        {
+            updateTxtboxUI.Start();
+            Txt03.Text = Thread.CurrentThread.Name;
+        }
+    }
+
+}
+
+// C# Quick Tip - Update UI Elements via Separate Thread
+// https://www.youtube.com/watch?v=9AIApJmbulY
